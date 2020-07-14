ffee2c7056fa3263561acf59d50669412570f2fd
梁啟立 <lvyaner@126.com>
2020-07-15 00:28:04 +0800

    修改注释

--- LeetCode/LeetCode/LeetCode04.swift
+++ LeetCode/LeetCode/LeetCode04.swift
@@ -132,8 +132,3 @@ class Solution04_3 {
         }
     }
 }
-
-作者：cjun-f0
-链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/-by-cjun-f0-2/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
